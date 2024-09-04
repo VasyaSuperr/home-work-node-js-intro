aef5d37d128c7507a6503b48b3ad1431e6bc0a27
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-09-04 16:26:34 +0300

    Add output of all files to the console

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const fs = require('node:fs');
 const fsPromises = require('node:fs/promises');
+const path = require('node:path');
 
 // ---------------------------------------------------------------------------
 // fs.readdir(__dirname, (err, files) => {
@@ -23,10 +24,29 @@ const fsPromises = require('node:fs/promises');
 
 // console.log('Test');
 // ---------------------------------------------------------------------------
-fsPromises
-  .readdir(__dirname, { encoding: 'utf-8' })
-  .then(files => console.log('Список файлів у поточній папці: ', files))
-  .catch(err => console.error('Помилка під час читання папки: ', err));
+// fsPromises
+//   .readdir(__dirname, { encoding: 'utf-8' })
+//   .then(files => console.log('Список файлів у поточній папці: ', files))
+//   .catch(err => console.error('Помилка під час читання папки: ', err));
 
-console.log('Test');
+// console.log('Test');
 // ---------------------------------------------------------------------------
+fs.readdir(__dirname, (err, files) => {
+  if (err) {
+    return console.error('Помилка під час читання папки: ', err);
+  }
+
+  const jsFilesFiltred = files.filter(f => path.extname(f) === '.js');
+
+  jsFilesFiltred.forEach(f => {
+    const filePath = path.join(__dirname, f);
+
+    fs.readFile(filePath, (err, file) => {
+      if (err) {
+        return console.error('Помилка під час читання файлу: ', err);
+      }
+      console.log(`Вміст файлу\n${file}: `);
+      console.log('==========================================================');
+    });
+  });
+});
