b6fbd2c263b924d29c24e2a6c454d55d344ae7d1
VasyaSuperr <yenakii.vasyl@gmail.com>
2024-09-04 16:15:06 +0300

    Add realisation readdir on the promises

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const fs = require('node:fs');
-const path = require('node:path');
+const fsPromises = require('node:fs/promises');
 
 // ---------------------------------------------------------------------------
 // fs.readdir(__dirname, (err, files) => {
@@ -13,13 +13,20 @@ const path = require('node:path');
 
 // console.log('Test');
 // ---------------------------------------------------------------------------
-try {
-  const files = fs.readdirSync(__dirname);
-  console.log('Список файлів у поточній папці: ');
-  files.forEach(f => console.log(f));
-} catch (error) {
-  console.error('Помилка під час читання папки: ', error);
-}
+// try {
+//   const files = fs.readdirSync(__dirname);
+//   console.log('Список файлів у поточній папці: ');
+//   files.forEach(f => console.log(f));
+// } catch (error) {
+//   console.error('Помилка під час читання папки: ', error);
+// }
+
+// console.log('Test');
+// ---------------------------------------------------------------------------
+fsPromises
+  .readdir(__dirname, { encoding: 'utf-8' })
+  .then(files => console.log('Список файлів у поточній папці: ', files))
+  .catch(err => console.error('Помилка під час читання папки: ', err));
 
 console.log('Test');
 // ---------------------------------------------------------------------------
